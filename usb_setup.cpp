939a1d81a1bc5b2dd2f4f11e4f0b6495b46b97f9
arpruss <arpruss@gmail.com>
2017-12-03 16:48:52 -0600

    disable usb serial

--- usb_setup.cpp
+++ usb_setup.cpp
@@ -38,6 +38,7 @@
 #define USB_HARDWARE
 #include "boards_private.h"
 
+#include "dummyserial.h"
 #include "usb_midi.h"
 #include "usb_midi_device.h"
 #include <libmaple/gpio.h>
@@ -46,9 +47,12 @@
 #include <boards.h>
 #include <usb_serial.h>
 
+
 namespace wirish {
     namespace priv {
 
+        static USBSerialNOP dummy;
+        
         void board_setup_usb(void) {
 			
 #ifdef GENERIC_BOOTLOADER			
@@ -65,9 +69,11 @@ namespace wirish {
 #endif
 #if  defined(USB_HARDWARE) && (defined(USB_HID_KMJ) || defined(USB_HID_KM) || defined(USB_HID_J))
 			HID.begin();
+            Serial = dummy;
 #endif
 #ifdef USB_MIDI
 			MidiUSB.begin();
+            Serial = dummy;
 #endif
 #endif
 		}
